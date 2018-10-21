e19b2ddc5795f5d93accefc2f494750d5c979acf
kurt <kurtlawrence92@gmail.com>
2018-10-21 16:10:37 +1000

    Adjusted build script to edit README.md rather than lib.rs

--- build.rs
+++ build.rs
@@ -1,49 +1,21 @@
-//! This build script will copy the `README.md` contents into both main.rs and lib.rs.
+//! This build script will copy the doc comments of `lib.rs` to `README.md`.
 
 use std::fs;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-	let (lib_str, main_str) = {
-		let readme = fs::read_to_string("README.md").unwrap();
-		let mainrs = fs::read("src/main.rs").unwrap();
-		let librs = fs::read("src/lib.rs").unwrap();
-		let readme_rdr = BufReader::new(readme.trim().as_bytes());
-		let mainrs_rdr = BufReader::new(&mainrs[..]);
-		let librs_rdr = BufReader::new(&librs[..]);
+	let lib = fs::read("src/lib.rs").unwrap();
+	let lib_rdr = BufReader::new(&lib[..]);
 
-		let mut mainrs_wtr = String::new();
-		let mut librs_wtr = String::new();
-		// println!("{}", String::from_utf8_lossy(&readme));
+	let mut wtr = String::new();
 
-		for line in readme_rdr.lines() {
-			let line = line.unwrap();
-			mainrs_wtr.push_str("//! ");
-			mainrs_wtr.push_str(&line);
-			mainrs_wtr.push('\n');
-			librs_wtr.push_str("//! ");
-			librs_wtr.push_str(&line);
-			librs_wtr.push('\n');
+	for line in lib_rdr.lines() {
+		let line = line.unwrap();
+		if line.starts_with("//!") {
+			let doc = line.trim_left_matches("//!").trim();
+			wtr.push_str(doc);
+			wtr.push('\n');
 		}
-
-		for line in mainrs_rdr.lines() {
-			let line = line.unwrap();
-			if !line.starts_with("//!") {
-				mainrs_wtr.push_str(&line);
-				mainrs_wtr.push('\n');
-			}
-		}
-		for line in librs_rdr.lines() {
-			let line = line.unwrap();
-			if !line.starts_with("//!") {
-				librs_wtr.push_str(&line);
-				librs_wtr.push('\n');
-			}
-		}
-
-		(librs_wtr, mainrs_wtr)
-	};
-
-	fs::write("src/main.rs", main_str).unwrap();
-	fs::write("src/lib.rs", lib_str).unwrap();
+	}
+	fs::write("README.md", wtr).unwrap();
 }
