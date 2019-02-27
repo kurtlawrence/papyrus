6314eba73664b918f44e214ca6ed9658f28e45a6
Kurt Lawrence <kurt@bne.precisionmining.com>
2019-02-28 01:47:30 +1000

    added code coverage for linking and pfh

--- src/pfh/linking.rs
+++ src/pfh/linking.rs
@@ -1,135 +1,135 @@
 //! Linking an external crate and sharing data.
-//! 
+//!
 //! When running a repl you might want to link an external crate. The specific use case is a developer wants to link the crate they are working on into the repl for the user to be able to use. A developer might also want to make data available to the repl. Papyrus has this functionality but makes some assumptions that the developer will need to be aware of, detailed below. When linking is desired, there are two main aspects to consider, the crate name to link and the data transferrence scheme (`Borrow` or `BorrowMut`).
-//! 
+//!
 //! The most simple linking is to make available an external crate (not through [`crates.io`](https://www.crates.io/)). There is no data transferrence, and as such simple chained functions on the `ReplData` structure can achieve this. There are two macros for the more complex data transferrence, `repl_data_brw!` and `repl_data_brw_mut!`.
-//! 
+//!
 //! There are four variations when using each macro, each applies to `repl_data_brw_mut!` as well.
-//! 
+//!
 //! ```ignore
 //! repl_data_brw!(crate_name, type);
 //! repl_data_brw!((crate_name, rlib_path), type);
 //! repl_data_brw!(compilation_dir, crate_name, type);
 //! repl_data_brw!(compilation_dir, (crate_name, rlib_path), type);
 //! ```
-//! 
+//!
 //! The `crate_name` is the string of the external crate to link, the optional addition of `rlib_path` forces Papyrus to copy the rlib from that location instead of searching for it. The `type` parameter is the type ascription, ie `String`, `MyStruct`, etc. Finally the `compilation_dir` defines a directory to use rather than the default.
-//! 
+//!
 //! # Worked Example - No Data
-//! 
+//!
 //! Let's work on a crate called `some-lib`.
-//! 
+//!
 //! ## File Setup
-//! 
+//!
 //! ***main.rs***:
-//! 
+//!
 //! ```ignore
 //! use papyrus::{Repl, ReplData};
-//! 
+//!
 //! fn main() {
 //!   let mut data = ReplData::default()
 //!     .with_extern_crate("some_lib", None)
 //!     .expect("failed linking crate")
 //! 	.no_extern_data();
 //!   let repl = Repl::default_terminal(&mut data);
-//! 
+//!
 //!   repl.run();
 //! }
 //! ```
-//! 
+//!
 //! ***lib.rs***:
-//! 
+//!
 //! ```rust
 //! pub struct MyStruct {
 //!   a: i32,
 //!   b: i32,
 //! }
-//! 
+//!
 //! impl MyStruct {
 //!   pub fn new(a: i32, b: i32) -> Self {
 //!   MyStruct { a, b }
 //!   }
-//! 
+//!
 //!   pub fn add_contents(&self) -> i32 {
 //!     self.a + self.b
 //!   }
 //! }
 //! ```
-//! 
+//!
 //! ***Cargo.toml***:
-//! 
+//!
 //! ```toml
 //! [package]
 //! name = "some-lib"
-//! 
+//!
 //! ...
-//! 
+//!
 //! [lib]
 //! name = "some_lib"
 //! crate-type = ["rlib", "staticlib"]
 //! path = "src/lib.rs" # you may need path to the library
-//! 
+//!
 //! [dependencies]
 //! papyrus = "*"
 //! ...
 //! ```
-//! 
+//!
 //! Notice that you will have to specify the library with a certain `crate-type`. Papyrus links using an `rlib` file, but I have shown that you can also build multiple library files.
-//! 
+//!
 //! If you build this project you should find a `libsome_lib.rlib` sitting in your build directory. Papyrus uses this to link when compiling.
-//! 
+//!
 //! ### Repl
-//! 
+//!
 //! Run this project (`cargo run`). It should spool up fine and prompt you with `papyrus=>`. Now you can try to use the linked crate.
-//! 
+//!
 //! ```sh
 //! papyrus=> some_lib::MyStruct::new(20, 30).add_contents()
 //! papyrus [out0]: 50
 //! ```
-//! 
+//!
 //! # What's going on
-//! 
+//!
 //! - Papyrus takes the crate name you specify and will add this as `extern crate CRATE_NAME;` to the source file.
 //! - When setting the external crate name, the `rlib` library is found and copied into the compilation directory.
 //!   - Papyrus uses `std::env::current_exe()` to find the executing folder, and searches for the `rlib` file in that folder (`libCRATE_NAME.rlib`)
 //!   - Specify the path to the `rlib` library if it is located in a different folder
 //! - When compiling the repl code, a rust flag is set, linking the `rlib` such that `extern crate CRATE_NAME;` works.
-//! 
+//!
 //! # Worked Example - Borrowed Data
-//! 
+//!
 //! Keep the example before, but alter the `main.rs` file.
-//! 
+//!
 //! ***main.rs***:
-//! 
+//!
 //! ```ignore
 //! extern crate somelib;
-//! 
+//!
 //! use somelib::MyStruct;
 //! use papyrus::{Repl, ReplData};
-//! 
+//!
 //! fn main() {
 //!   let my_struct = MyStruct::new(20,30);
-//! 
+//!
 //!   let mut data = repl_data_brw!("somelib", MyStruct).expect("failed creating data");
 //!   let repl = Repl::default_terminal(&mut data);
-//! 
+//!
 //!   repl.run();
 //! }
 //! ```
-//! 
+//!
 //! Run this project (`cargo run`). It should spool up fine and prompt you with `papyrus=>`. Now you can try to use the linked data. The linked data is in a variable `app_dat` and depending of the variant it will be `&` or `&mut`.
-//! 
+//!
 //! ```sh
 //! papyrus=> app_data.add_contents()
 //! papyrus [out0]: 50
 //! ```
-//! 
+//!
 //! This will work with `BorrowMut` variants as well.
-//! 
+//!
 //! # Notes
-//! 
+//!
 //! ## Panics
-//! 
+//!
 //! To avoid crashing the application on a panic, `catch_unwind` is employed. This function requires data that crosses the boundary be `UnwindSafe`, making `&` and `&mut` not valid data types. Papyrus uses `AssertUnwindSafe` wrappers to make this work, however it makes `app_data` vunerable to breaking invariant states if a panic is triggered. In practice the repl is designed to be low imapct and such should not have many cases where broken invariants are caused, however there is no garauntee.
 
 use std::path::{Path, PathBuf};
@@ -152,10 +152,11 @@ mod macros {
 				);
 			repl_data_res
 			}};
-		// ((crate_name, rlib_path), type)
-		($crate_and_rlib:expr, $type:ty) => {{
+		// (crate_name, rlib_path, type)
+		($crate_name:expr, $rlib_path:expr, $type:ty) => {{
 			use papyrus;
-			let (crate_name, rlib_path): (&'static str, &str) = $crate_and_rlib;
+			let crate_name: &str = $crate_name;
+			let rlib_path: &str = $rlib_path;
 			let repl_data_res: std::io::Result<
 				papyrus::ReplData<_, papyrus::linking::BorrowData, $type>,
 			> = papyrus::ReplData::default().with_extern_crate_and_data(
@@ -165,8 +166,8 @@ mod macros {
 				);
 			repl_data_res
 			}};
-		// (compilation_dir, crate_name, type)
-		($comp_dir:expr, $crate_name:expr, $type:ty) => {{
+		// (crate_name, type, compilation_dir)
+		($crate_name:expr, $type:ty, $comp_dir:expr) => {{
 			use papyrus;
 			let compilation_dir: &str = $comp_dir;
 			let crate_name: &'static str = $crate_name;
@@ -178,11 +179,12 @@ mod macros {
 				Err(e) => Err(e),
 				}
 			}};
-		// (compilation_dir, (crate_name, rlb_path), type)
-		($comp_dir:expr, $crate_and_rlib:expr, $type:ty) => {{
+		// (crate_name, rlb_path, type, compilation_dir)
+		($crate_name:expr, $rlib_path:expr, $type:ty, $comp_dir:expr) => {{
 			use papyrus;
 			let compilation_dir: &str = $comp_dir;
-			let (crate_name, rlib_path): (&'static str, &str) = $crate_and_rlib;
+			let crate_name: &str = $crate_name;
+			let rlib_path: &str = $rlib_path;
 			let repl_data_res: std::io::Result<
 				papyrus::ReplData<_, papyrus::linking::BorrowData, $type>,
 			> = papyrus::ReplData::default().with_compilation_dir(compilation_dir);
@@ -211,10 +213,11 @@ mod macros {
 				);
 			repl_data_res
 			}};
-		// ((crate_name, rlib_path), type)
-		($crate_and_rlib:expr, $type:ty) => {{
+		// (crate_name, rlib_path, type)
+		($crate_name:expr, $rlib_path:expr, $type:ty) => {{
 			use papyrus;
-			let (crate_name, rlib_path): (&'static str, &str) = $crate_and_rlib;
+			let crate_name: &str = $crate_name;
+			let rlib_path: &str = $rlib_path;
 			let repl_data_res: std::io::Result<
 				papyrus::ReplData<_, papyrus::linking::BorrowMutData, $type>,
 			> = papyrus::ReplData::default().with_extern_crate_and_data(
@@ -224,8 +227,8 @@ mod macros {
 				);
 			repl_data_res
 			}};
-		// (compilation_dir, crate_name, type)
-		($comp_dir:expr, $crate_name:expr, $type:ty) => {{
+		// (crate_name, type, compilation_dir)
+		($crate_name:expr, $type:ty, $comp_dir:expr) => {{
 			use papyrus;
 			let compilation_dir: &str = $comp_dir;
 			let crate_name: &'static str = $crate_name;
@@ -237,11 +240,12 @@ mod macros {
 				Err(e) => Err(e),
 				}
 			}};
-		// (compilation_dir, (crate_name, rlb_path), type)
-		($comp_dir:expr, $crate_and_rlib:expr, $type:ty) => {{
+		// (crate_name, rlb_path, type, compilation_dir)
+		($crate_name:expr, $rlib_path:expr, $type:ty, $comp_dir:expr) => {{
 			use papyrus;
 			let compilation_dir: &str = $comp_dir;
-			let (crate_name, rlib_path): (&'static str, &str) = $crate_and_rlib;
+			let crate_name: &str = $crate_name;
+			let rlib_path: &str = $rlib_path;
 			let repl_data_res: std::io::Result<
 				papyrus::ReplData<_, papyrus::linking::BorrowMutData, $type>,
 			> = papyrus::ReplData::default().with_compilation_dir(compilation_dir);
@@ -253,6 +257,19 @@ mod macros {
 				}
 			}};
 	}
+
+	// FIXME enable this at some point?
+	// #[test]
+	// fn macros_test() {
+	// 	repl_data_brw!("crate_name", String);
+	// 	repl_data_brw!("crate_name", "some/path/to/rlib", String);
+	// 	repl_data_brw!("crate_name", String, "compile_dir");
+	// 	repl_data_brw!("crate_name", "some/path/to/rlib", String, "compile_dir");
+	// 	repl_data_brw_mut!("crate_name", String);
+	// 	repl_data_brw_mut!("crate_name", "some/path/to/rlib", String);
+	// 	repl_data_brw_mut!("crate_name", String, "compile_dir");
+	// 	repl_data_brw_mut!("crate_name", "some/path/to/rlib", String, "compile_dir");
+	// }
 }
 
 pub struct LinkingConfiguration {
@@ -284,8 +301,6 @@ impl LinkingConfiguration {
 			Some(p) => PathBuf::from(p),
 			None => get_rlib_path(crate_name)?,
 		};
-
-		dbg!(&rlib_path);
 
 		fs::copy(
 			rlib_path,
@@ -332,7 +347,6 @@ pub enum LinkingArgument {
 fn get_rlib_path(crate_name: &str) -> io::Result<PathBuf> {
 	let lib_name = format!("lib{}.rlib", crate_name);
 	let exe = std::env::current_exe()?;
-	dbg!(&exe);
 	fs::read_dir(exe.parent().expect("files should always have a parent"))?
 		.into_iter()
 		.filter(|entry| entry.is_ok())
@@ -342,4 +356,62 @@ fn get_rlib_path(crate_name: &str) -> io::Result<PathBuf> {
 			io::ErrorKind::NotFound,
 			format!("did not find file: '{}'", lib_name),
 		))
+}
+
+#[test]
+fn get_rlib_path_test() {
+	use std::error::Error;
+	let r = get_rlib_path("some_crate");
+	assert!(r.is_err());
+	let e = r.unwrap_err();
+	assert_eq!(e.kind(), io::ErrorKind::NotFound);
+	assert_eq!(e.description(), "did not find file: 'libsome_crate.rlib'");
+}
+
+#[test]
+fn linking_config_test() {
+	let dir = PathBuf::from("test/linking_config_test/");
+	fs::create_dir_all(&dir).unwrap();
+	let lc = LinkingConfiguration::link_external_crate(&dir, "some_crate", None);
+	assert!(lc.is_err());
+	fs::write(dir.join("asdf.txt"), "").unwrap();
+	let lc = LinkingConfiguration::link_external_crate(
+		&dir,
+		"some_crate",
+		Some("test/linking_config_test/asdf.txt"),
+	)
+	.unwrap();
+	assert_eq!(lc.crate_name, "some_crate");
+	assert_eq!(lc.data_type, None);
+
+	// test no data type fn args
+	assert_eq!(
+		lc.construct_fn_args(&LinkingArgument::NoData),
+		String::new()
+	);
+	assert_eq!(
+		lc.construct_fn_args(&LinkingArgument::BorrowData),
+		String::new()
+	);
+	assert_eq!(
+		lc.construct_fn_args(&LinkingArgument::BorrowMutData),
+		String::new()
+	);
+
+	let lc = lc.with_data("String");
+	assert_eq!(lc.data_type, Some("String".to_string()));
+
+	// test data type fn args
+	assert_eq!(
+		lc.construct_fn_args(&LinkingArgument::NoData),
+		String::new()
+	);
+	assert_eq!(
+		&lc.construct_fn_args(&LinkingArgument::BorrowData),
+		"app_data: &some_crate::String"
+	);
+	assert_eq!(
+		&lc.construct_fn_args(&LinkingArgument::BorrowMutData),
+		"app_data: &mut some_crate::String"
+	);
 }
--- src/pfh/mod.rs
+++ src/pfh/mod.rs
@@ -14,4 +14,12 @@ pub fn eval_fn_name(mod_path: &[String]) -> String {
 	format!("_{}_intern_eval", mod_path.join("_"))
 }
 
-
+#[test]
+fn eval_fn_name_test() {
+	let path: Vec<String> = ["some", "lib", "module", "path"]
+		.iter()
+		.map(|x| x.to_string())
+		.collect();
+	assert_eq!(&eval_fn_name(&path), "_some_lib_module_path_intern_eval");
+	assert_eq!(&eval_fn_name(&[]), "__intern_eval");
+}
