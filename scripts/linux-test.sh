8a0a1f5f3441af7f6dda3d9e74b4a9c02e8550b8
Kurt Lawrence <kurt@bne.precisionmining.com>
2019-03-01 01:51:07 +1000

    split out linux and windows testing scripts

---
+++ scripts/linux-test.sh
@@ -0,0 +1,10 @@
+cd ../azul
+git pull
+cd ../papyrus
+git pull  # maybe git reset --hard master if Cargo.lock playing up
+cd test-resources/external_crate
+cargo build
+cd ../..
+cargo test -- --test-threads=1
+cargo tarpaulin -v -l -out Html
+mv tarpaulin-report.html /mnt/c/users/kurt/desktop/tarpaulin-report.html
