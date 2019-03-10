e35fe4fba5570f9d52b70653fe76c3aa5f56000b
Kurt Lawrence <kurt@bne.precisionmining.com>
2019-03-10 22:09:22 +1000

    update dependencies

--- scripts/linux-test.sh
+++ scripts/linux-test.sh
@@ -9,3 +9,9 @@ cd ../..
 cargo test -- --test-threads=1
 cargo tarpaulin -v -l --out Html
 mv tarpaulin-report.html /mnt/c/users/kurt/desktop/tarpaulin-report.html
+
+wdir="$(pwd)" # get working directory
+cd /mnt/c/users/kurt/desktop # switch to windows desktop
+./tarpaulin-html-converter.exe # run the converter
+cd "$wdir" # switch back to old dir
+echo "report converted!"
