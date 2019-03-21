3d6ea6966db11d91b42093fce31551505f7ef316
Kurt Lawrence <kurt@bne.precisionmining.com>
2019-03-21 17:21:44 +0200

    fixed async example

--- examples/async.rs
+++ examples/async.rs
@@ -8,7 +8,9 @@ fn main() {
 
 	let mut v = Arc::new(Mutex::new(String::new()));
 
-	let line = "std::thread::sleep_ms(5000); app_data.push_str(\"Hello, world!\"); app_data\n";
+	// notice the .mut\n putting into mutating state
+	let line =
+		".mut\nstd::thread::sleep_ms(5000); app_data.push_str(\"Hello, world!\"); app_data\n";
 
 	let mut slice = line;
 	while slice.len() > 0 {
@@ -25,5 +27,4 @@ fn main() {
 
 	let v_lock = v.lock().unwrap();
 	assert_eq!(&v_lock.to_string(), "Hello, world!");
-	// TODO this is currently broken -- app_data is no longer mutable, but if mechanism in place to mutate might alter.
 }
