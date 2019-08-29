369dd98f78177cfe6d6bf1ae65f29f0bde0732cd
Kurt Lawrence <kurtlawrence92@gmail.com>
2019-08-29 15:47:53 -0600

    docs: move book to docs/ folder

--- papyrus/feature-check.cmd
+++
@@ -1,5 +0,0 @@
-cargo check
-cargo check --all-features
-cargo check --no-default-features
-cargo check --no-default-features --features runnable
-cargo check --no-default-features --features racer-completion
