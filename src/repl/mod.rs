38c4ce4ccb4418cc5f91e54c4586dd3f9259999a
Kurt Lawrence <kurtlawrence92@gmail.com>
2020-01-07 21:09:47 +1100

    fmt

--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -201,7 +201,7 @@ fn test_default_compile_dir() {
     assert!(dir.ends_with(".papyrus/"));
     if cfg!(windows) {
         assert!(dir.starts_with("C:\\Users\\"));
-    } else if cfg!(target_os = "macos"){
+    } else if cfg!(target_os = "macos") {
         assert!(dir.starts_with("/Users/"));
     } else {
         assert!(dir.starts_with("/home/"));
