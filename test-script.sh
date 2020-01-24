6124d5c32fbd844b7aee1195f850c347ca5d9410
kurtlawrence <kurtlawrence92@gmail.com>
2020-01-24 10:24:49 +1000

    Fix backtrace (#76)
    * require backtrace crate to use std feature
    
    * add the test-script into vc

---
+++ test-script.sh
@@ -0,0 +1,32 @@
+set -e
+
+# Build external_crate
+cd test-resources/external_crate
+cargo clean
+cargo build
+cd ../..
+# Build external_kserd
+cd test-resources/external_kserd
+cargo clean
+cargo build
+cd ../..
+
+# Update docs
+cargo modoc
+cargo +stable fmt
+
+# Check formatting
+cargo +stable fmt -- --check
+
+# Check clippy
+# cargo clippy --all-features -- -D warnings 
+
+# Run tests; includes interface testing
+cargo test --all-features -- --test-threads=1
+
+# Run cargo docs and ensure the linking is working
+cargo doc
+
+# To build docs, need clean target directory
+# mdbook build docs
+# mdbook test -L target/debug,target/debug/deps docs
