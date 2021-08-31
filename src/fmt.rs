2ea0444f65118d5890e24092bc1f88b11f2950d7
Sam 'Gyro' C <gyrocoder@gmail.com>
2021-08-31 15:32:51 -0400

    Fix formatting issue.

--- src/fmt.rs
+++ src/fmt.rs
@@ -47,6 +47,9 @@ pub fn format(code_snippet: &str) -> Result<String, FormatError> {
     if success && !outputbuf.is_empty() {
         let s = std::str::from_utf8(&outputbuf).map_err(|_| FormatError::StrConvertFailed)?;
 
+        //Protect against a .rustfmt.toml with "hard_tabs = true" in it. 
+        let s = s.replace("\t", "    ");
+        
         let trimmed = s.trim();
         let end = trimmed.len().saturating_sub(2); // \n}
 
